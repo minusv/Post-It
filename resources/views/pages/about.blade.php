293cf8fd66462a6273b07c5501336775dcbf659a
Manasvi Saxena <msmanasup@gmail.com>
2018-12-06 16:28:11 +0530

    PagesController Created

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>firstLaravelApp</title>
+    </head>
+    <body>
+        <h1> About </h1>
+        <p> Trying my hands on Laravel. </p>
+    </body>
+</html>
